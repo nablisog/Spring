470b9fc007daedfed6f24add2a29a55bbeef0a19
nablisog <42609393+nablisog@users.noreply.github.com>
2022-10-19 13:39:23 +0200

    Delete SpringBoot MVC/springMongodb directory

--- SpringBoot MVC/springMongodb/src/main/java/Mongodb/repository/SubjectRepository.java
+++
@@ -1,9 +0,0 @@
-package Mongodb.repository;
-
-import Mongodb.entitiy.Subject;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubjectRepository extends MongoRepository<Subject, String> {
-}
