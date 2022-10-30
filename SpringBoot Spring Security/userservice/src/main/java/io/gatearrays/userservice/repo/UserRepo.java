edc8b7da5398505e8f44c9d11d4e675703a63385
nablisog <42609393+nablisog@users.noreply.github.com>
2022-10-30 23:07:33 +0100

    Delete SpringBoot Spring Security/userservice directory

--- SpringBoot Spring Security/userservice/src/main/java/io/gatearrays/userservice/repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package io.gatearrays.userservice.repo;
-
-import io.gatearrays.userservice.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
