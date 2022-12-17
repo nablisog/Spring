bdd825ac06eec03d3da993dd0df1470dfcc01051
nablisog <42609393+nablisog@users.noreply.github.com>
2022-12-17 15:59:42 +0100

    Delete SpringBoot MVC/Shippment/springboot-mongodb directory

--- SpringBoot MVC/Shippment/springboot-mongodb/src/main/java/com/example/springbootmongodb/repository/ShoppingCartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootmongodb.repository;
-
-import com.example.springbootmongodb.entity.ShoppingCart;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShoppingCartRepository extends MongoRepository<ShoppingCart,String> {
-}
